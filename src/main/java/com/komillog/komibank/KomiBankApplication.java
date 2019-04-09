65c284d7379484a48d0f2b1229355588548a34e4
Léon Logli <leonlogli@gmail.com>
2019-04-10 00:42:42 +0100

    Project creation: initial commit

---
+++ src/main/java/com/komillog/komibank/KomiBankApplication.java
@@ -0,0 +1,13 @@
+package com.komillog.komibank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KomiBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KomiBankApplication.class, args);
+	}
+
+}
