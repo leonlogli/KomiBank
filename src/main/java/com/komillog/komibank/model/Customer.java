c1b97ac9d3067f2bd63d824693d3fdda5be94e2a
Léon Logli <leonlogli@gmail.com>
2019-04-10 01:38:26 +0100

    Customer entity
    Creation of constructors, properties, getters and setters

--- src/main/java/com/komillog/komibank/model/Customer.java
+++ src/main/java/com/komillog/komibank/model/Customer.java
@@ -1,7 +1,84 @@
 package com.komillog.komibank.model;
 
 import java.io.Serializable;
+import java.util.List;
 
+/**
+ * A bank customer
+ * 
+ * @author KomiLLog
+ *
+ */
 public class Customer implements Serializable {
 	
+	private Long id;
+	private String name;
+	private String email;
+	
+	private List<Account> accounts;
+	
+	
+    /***************************************************************************
+     *                                                                         *
+     * Constructors                                                            *
+     *                                                                         *
+     **************************************************************************/
+	
+	/**
+	 * Create a new customer
+	 */
+	public Customer() {
+		super();
+	}
+
+	/**
+	 * Create a new customer with the specified {@code name} and {@code email}
+	 * 
+	 * @param name customer's name
+	 * @param email customer's email
+	 */
+	public Customer(String name, String email) {
+		super();
+		this.name = name;
+		this.email = email;
+	}
+	
+
+    /***************************************************************************
+     *                                                                         *
+     * Setters and Getters                                                     *
+     *                                                                         *
+     **************************************************************************/
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public List<Account> getAccounts() {
+		return accounts;
+	}
+
+	public void setAccounts(List<Account> accounts) {
+		this.accounts = accounts;
+	}
 }
