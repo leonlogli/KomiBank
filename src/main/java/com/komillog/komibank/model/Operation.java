65552b48f061329b08b4e7147bfbddd113d9906c
Léon Logli <leonlogli@gmail.com>
2019-04-10 02:39:18 +0100

    Operation entity
    Creation of constructors, properties, getters and setters

--- src/main/java/com/komillog/komibank/model/Operation.java
+++ src/main/java/com/komillog/komibank/model/Operation.java
@@ -1,7 +1,99 @@
 package com.komillog.komibank.model;
 
 import java.io.Serializable;
+import java.util.Date;
 
+/**
+ * Abstract class for banking operations
+ * 
+ * @author KomiLLog
+ *
+ */
 public abstract class Operation implements Serializable {
+	
+	private Long id;
+	private double amount;
+	private Date date;
+	
+	private Account account;
+	
+
+	/***************************************************************************
+     *                                                                         *
+     * Constructors                                                            *
+     *                                                                         *
+     **************************************************************************/
+	
+	/**
+	 * Create a new operation
+	 */
+	public Operation() {
+		super();
+	}
+
+	/**
+	 * Create a new operation with the specified parameters
+	 * 
+	 * @param amount operation amount
+	 * @param date operation date
+	 * @param account the account concerned by this Operation
+	 */
+	public Operation(double amount, Date date, Account account) {
+		super();
+		this.amount = amount;
+		this.date = date;
+		this.account = account;
+	}
+	
+	/***************************************************************************
+     *                                                                         *
+     * Abstract methods                                                       *
+     *                                                                         *
+     **************************************************************************/
+	
+	/**
+	 * Returns the string representation of the operation type
+	 * @return the string representation of the operation type
+	 */
+	protected abstract String type();
+	
+	
+	/***************************************************************************
+     *                                                                         *
+     * Setters and Getters                                                     *
+     *                                                                         *
+     **************************************************************************/
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Account getAccount() {
+		return account;
+	}
+
+	public void setAccount(Account account) {
+		this.account = account;
+	}
 	
 }
