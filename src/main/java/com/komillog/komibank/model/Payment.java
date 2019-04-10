638ad7cc21070dd3a45cca9a203e7a938063f494
Léon Logli <leonlogli@gmail.com>
2019-04-10 03:23:55 +0100

    Payment entity
    Creation of constructors, overidable methods

--- src/main/java/com/komillog/komibank/model/Payment.java
+++ src/main/java/com/komillog/komibank/model/Payment.java
@@ -1,5 +1,38 @@
 package com.komillog.komibank.model;
 
+import java.util.Date;
+
+/**
+ * A bank Payment operation
+ * 
+ * @author KomiLLog
+ *
+ */
 public class Payment extends Operation {
 
+	/**
+	 * Create a new payment operation 
+	 */
+	public Payment() {
+		super();
+	}
+
+	/**
+	 * Create a new payment operation with the specified parameters
+	 * 
+	 * @param amount the payment amount
+	 * @param date payment date
+	 * @param account the account on which the amount will be paid
+	 */
+	public Payment(double amount, Date date, Account account) {
+		super(amount, date, account);
+	}
+	
+	/***
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected String type() {
+		return "Payment";
+	}
 }
