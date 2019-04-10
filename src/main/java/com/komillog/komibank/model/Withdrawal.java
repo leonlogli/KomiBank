7f33fe1c3d5cc25d2b45d0117ea5eadfdf976486
Léon Logli <leonlogli@gmail.com>
2019-04-10 03:14:55 +0100

    WIthdrawal entity
    Creation of constructors, overidable methods

--- src/main/java/com/komillog/komibank/model/Withdrawal.java
+++ src/main/java/com/komillog/komibank/model/Withdrawal.java
@@ -1,5 +1,39 @@
 package com.komillog.komibank.model;
 
+import java.util.Date;
+
+/**
+ * A bank withdrawal operation
+ * 
+ * @author KomiLLog
+ *
+ */
 public class Withdrawal extends Operation {
+
+	/**
+	 * Create a new withdrawal operation
+	 */
+	public Withdrawal() {
+		super();
+	}
+
+	/**
+	 * Create a new withdrawal operation with the specified parameters
+	 * 
+	 * @param amount withdrawal amount
+	 * @param date withdrawal date
+	 * @param account the account on which the amount will be withdrawn
+	 */
+	public Withdrawal(double amount, Date date, Account account) {
+		super(amount, date, account);
+	}
+	
+	/***
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected String type() {
+		return "Withdrawal";
+	}
 
 }
