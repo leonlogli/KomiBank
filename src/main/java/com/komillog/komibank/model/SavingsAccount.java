2dc55602d282e30910a1f93467b80e252b00279c
Léon Logli <leonlogli@gmail.com>
2019-04-10 02:23:08 +0100

    SavingsAccount entity
    Creation of constructors, properties, getters and setters

--- src/main/java/com/komillog/komibank/model/SavingsAccount.java
+++ src/main/java/com/komillog/komibank/model/SavingsAccount.java
@@ -1,5 +1,51 @@
 package com.komillog.komibank.model;
 
+import java.util.Date;
+
+/**
+ * Savings Account
+ * 
+ * @author KomiLLog
+ *
+ */
 public class SavingsAccount extends Account {
 
+	private double interestRate;
+
+	/**
+	 * Create a new savings account
+	 */
+	public SavingsAccount() {
+		super();
+	}
+	
+	/**
+	 * Create a new savings account with the specified parameters
+	 * 
+	 * @param code the code of the account
+	 * @param customer the customer who owns this account
+	 * @param balance account balance
+	 * @param creationDate the account creation date
+	 * @param interestRate the interest rate of the account
+	 */
+	public SavingsAccount(String code, Customer customer, double balance, Date creationDate, double interestRate) {
+		super(code, customer, balance, creationDate);
+		this.interestRate = interestRate;
+	}
+	
+
+	/***************************************************************************
+     *                                                                         *
+     * Setters and Getters                                                     *
+     *                                                                         *
+     **************************************************************************/
+
+	public double getInterestRate() {
+		return interestRate;
+	}
+
+	public void setInterestRate(double interestRate) {
+		this.interestRate = interestRate;
+	}
+	
 }
