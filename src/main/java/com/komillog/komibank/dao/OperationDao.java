87b8d24d2b5b8301f1208b93472177ee2ecba4f1
Léon Logli <leonlogli@gmail.com>
2019-04-11 08:23:54 +0100

    additional method "getAccountOperations" in OperationDAO repository

--- src/main/java/com/komillog/komibank/dao/OperationDao.java
+++ src/main/java/com/komillog/komibank/dao/OperationDao.java
@@ -1,10 +1,17 @@
 package com.komillog.komibank.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.komillog.komibank.model.Operation;
 
 
 public interface OperationDao extends JpaRepository<Operation, Long>{
 
+	@Query("select op from Operation op where op.account.code=:x order by op.date desc")
+	Page<Operation> getAccountOperations(@Param("x") String accountCode, Pageable pageable);
+	
 }
