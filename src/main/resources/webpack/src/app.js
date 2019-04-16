5109870751bcf0cb42cad0254445987a3fc085bc
Léon Logli <leonlogli@gmail.com>
2019-04-16 09:10:13 +0100

    Init ripple effect on Material components

--- src/main/resources/webpack/src/app.js
+++ src/main/resources/webpack/src/app.js
@@ -1,1 +1,8 @@
+import {MDCRipple} from '@material/ripple/index';
 import './sass/app.scss';
+
+// Setup a ripple effect for mdc compoents
+const selector = '.mdc-button, .mdc-icon-button, .mdc-card__primary-action';
+const ripples = [].map.call(document.querySelectorAll(selector), function(item) {
+    return new MDCRipple(item);
+});
